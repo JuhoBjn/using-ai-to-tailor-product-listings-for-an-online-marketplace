b767f13a319730c44c5642ff3381a13a3e885993
Juho Björkman <juho.bjorkman@tuni.fi>
2023-11-14 02:02:21 +0200

    Add backdrop

---
+++ frontend/src/pages/store/components/backdrop/Backdrop.jsx
@@ -0,0 +1,12 @@
+import ReactDOM from "react-dom";
+
+import "./Backdrop.css";
+
+const Backdrop = ({ onClick }) => {
+  return ReactDOM.createPortal(
+    <div className="backdrop" onClick={onClick} />,
+    document.getElementById("backdrop-hook")
+  );
+};
+
+export default Backdrop;
